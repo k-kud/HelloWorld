a46b404b1cf5d7e3f9267716d90ac72ca1b6c8d9
ayumu-nitanai <a.nitanai1811@morijyobi.ac.jp>
2019-06-04 11:20:56 +0900

    [modify] Conflict.java

--- Conflict.java
+++ Conflict.java
@@ -1,5 +1,11 @@
-public class HelloWorld{
-	public static void main(String[] args){
-		System.out.println("HelloWorld");
-	}
+iimport java.util.Random;
+public class Ransu {
+  public static void main(String args[]) {
+    Random rand = new Random();
+    for(int i=0; i<10; i++){
+
+      int d  = rand.nextInt(100) +1 ;
+    System.out.println(d);
+    }
+  }
 }
